08412b3e4b32ede2fbb361ba41db6181921d3c8f
Vladimir Penzov <43757268+boername@users.noreply.github.com>
2019-03-27 19:59:38 +0300

    Update moylsp.lisp

--- moylsp.lisp
+++ moylsp.lisp
@@ -28,3 +28,18 @@
 ;Задача 12 Определите функцию, заменяющую в исходном списке два подряд идущих
 ;одинаковых элемента одним.
 
+(defun zamen (lst)
+  ((lambda ( x y )    
+        (cond ((null y) lst)
+             ((equal x (cadr lst)) (cons x (zamen (cddr lst))))
+             (t (cons x (zamen y )))))  
+  (car lst) 
+  (cdr lst )))
+
+
+    ;(print (zamen '( a a b b c c c)))
+    ;(A B C C)
+    ;(print (zamen '( a a (b b c) (b b c) (b b c) c f f c)))
+    ;(A (B B C) (B B C) C F C) 
+    ;(print (zamen '(1 1 1 1 1 1 4 5 67 7 7)))
+    ;(1 1 1 4 5 67 7)
