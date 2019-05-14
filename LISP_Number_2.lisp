e92a23066e9a37fce9d6bac198c590fcc5590bbe
Vladimir Penzov <43757268+boername@users.noreply.github.com>
2019-05-14 19:40:08 +0300

    Update and rename LISP_Number_2 to LISP_Number_2.lisp

--- LISP_Number_2.lisp
+++ LISP_Number_2.lisp
@@ -1,6 +1,15 @@
 ;---------------------------------------------------Вариант 28------------------------------------------------------------------
 ;Задача 2. Определите функционал (MAPLIST fn список) для одного списочного аргумента.
 
+(defun maplistSpecial (fn list)
+	(cond
+		((null list) nil)
+		(t (cons (funcall fn list) (maplistSpecial fn (cdr list))))
+	)
+)
+
+(print (maplistSpecial (lambda (x) (apply '+ x)) '(1 2 3 4 5)))
+;(15 14 12 9 5)
 
 
 
