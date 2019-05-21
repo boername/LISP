fac60679301622f960000f7a0e0b798e5004d8f0
Vladimir Penzov <43757268+boername@users.noreply.github.com>
2019-05-21 16:38:33 +0300

    Update macro.lisp

--- macro.lisp
+++ macro.lisp
@@ -1,1 +1,24 @@
+;1. Определите макрос, который возвращает свой вызов.
 
+(defmacro macros1 (&rest x)
+    `'(macros1 ,@x))
+ 
+(print (macros1 7 77 3 4 5 67 1)) 
+
+
+; (macros1 7 77 3 4 5 67 1)
+
+
+
+;3. Определите лисповскую форму (IF условие p q) в виде макроса.
+
+(defmacro macroIF (условие р q)
+    `(if ,условие ,р ,q))
+    
+    
+(setq x '(a b c 1 2 3))
+
+(print (macroIF (atom x) 'T 'F))
+;F
+(print (macroIF (listp x) 'T 'F))
+;T
