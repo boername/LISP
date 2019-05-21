a1ecfa9bdf97db3dab8368eed1d1abf2641f295b
Vladimir Penzov <43757268+boername@users.noreply.github.com>
2019-05-21 19:25:31 +0300

    Update haskell.hs

--- haskell.hs
+++ haskell.hs
@@ -11,9 +11,21 @@ main = print $ sumL [1, 2, 3, 4, 5, 6, 7 ,8 ,9]
 
 --2. Реализовать на языке Haskell функцию нахохдения максимального элемента списка.
 
-maxElement []=0
-maxElement (head:tail)=max head (maxElement tail)
+maxElement [] = 0
+maxElement (head:tail) = max head (maxElement tail)
 
 main = print $ maxElement [1, 2, 3, 4, 5, 6, 7, 8, 9]
 --9
+
+
+
+--3. Реализовать на языке Haskell функцию, удаляющую из исходного списка элементы с четными номерами.
+
+deleteEven [] = []
+deleteEven (head:tail) =  head : deleteEven (drop 1 tail)
+
+
+main = print $ deleteEven [1, 2, 3, 4, 5, 6 ,7, 8, 9]
+--[1,3,5,7,9]
+
 
